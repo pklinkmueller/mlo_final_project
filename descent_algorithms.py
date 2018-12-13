adf6d396538387b2f180edbd74e2d3e8a71ad602
pklinkmueller <pklinkmueller@jhu.edu>
2018-12-12 20:15:26 -0500

    Change dimension mismatches for SVRG.

--- descent_algorithms.py
+++ descent_algorithms.py
@@ -38,31 +38,31 @@ class GradientDescent(DescentAlgorithm):
 class StochasticVarianceReducedGradientDescent(DescentAlgorithm):
     def __init__(self, eta: LearningRate, k: int):
         self.eta = eta
-        self.w_est = np.rand(k)
+        self.w_est = np.random.rand(1,k) * 10
     
     def update(self, model, X, y):
         w_est_curr = self.w_est
-        mean_est = zero(w_est_curr.shape)
+        mean_est = np.zeros(w_est_curr.shape)
         n = X.shape[0]
         
         model.w = self.w_est
         for i in range(0,n):
-            sample = X[i,:]
+            sample = X[i,:].reshape(X.shape[1],1)
             mean_est += model.grad(sample, y[i])
         mean_est /= n
         
         w_0 = w_est_curr
         for t in range(0,2*n):
-            i_t = randint(0,n+1)
-            sample = X[i_t,:]
+            i_t = randint(0,n-1)
+            sample = X[i_t,:].reshape(X.shape[1],1)
             model.w = w_0
             local_grad = model.grad(sample, y[i_t])
             model.w = w_est_curr
             global_grad = model.grad(sample, y[i_t])
-            w_0 = w_0 - self.eta * (local_grad - global_grad + mean_est)
+            w_0 = w_0 - self.eta.get_rate() * (local_grad - global_grad + mean_est)
         self.w_est = w_0
         
-        return self.w_est
+        return np.transpose(self.w_est)
 
 class NesterovAcceleratedDescent(DescentAlgorithm):
     def __init__(self, eta: LearningRate, y0: np.ndarray):
