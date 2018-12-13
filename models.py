4ed2da7ac891deba31a2aba7bec23c4420a7d90f
pklinkmueller <pklinkmueller@jhu.edu>
2018-12-13 02:40:14 -0500

    Changed training to output full train loss measures. Added outline for MSE (if we ever use...).

--- models.py
+++ models.py
@@ -28,6 +28,29 @@ class Model:
     def grad(self, X: np.ndarray, y: np.ndarray):
         raise NotImplementedError
 
+# class LinearRegression(Model):
+#     def __init__(self, descent: DescentAlgorithm, lr: LearningRate, num_iter: int, batch_size: int):
+#         super().__init__(descent, lr, num_iter, batch_size)
+
+#     @staticmethod
+#     def __sigmoid(z):
+#         return 1 / (1 + np.exp(-z))
+
+#     def loss(self, h, y):
+#         return np.sum((h - y)**2) / y.shape[0]
+
+#     def grad(self, X, y):
+#         h = self.predict(X)
+#         return np.dot(X.T, (h - y)) / y.shape[0]
+
+#     def fit(self, X: np.ndarray, y: np.ndarray):
+#         self.w = np.random.rand(X.shape[1], 1)
+#         print(self.w.shape)
+#         loss_data = train(X, y, self, int(self.num_iter / 10))
+#         return loss_data
+
+#     def predict(self, X: np.ndarray):
+#         return self.__sigmoid(np.dot(X, self.w))
 
 class LogisticRegression(Model):
     def __init__(self, descent: DescentAlgorithm, lr: LearningRate, num_iter: int, batch_size: int):
@@ -68,9 +91,9 @@ def train(X: np.ndarray, y: np.ndarray, model: Model, print_iter: int) -> np.nda
         bh = model.predict(bX)
         model.w = model.descent.update(model, X, y)
         start_idx = stop_idx % n
-        loss_data[i] = model.loss(bh, bY)
+        loss_data[i] = model.loss(model.predict(X), y)
         if i % print_iter == 0:
-            print('Iter: {:8} batch loss: {:.3f}'.format(i, float(loss_data[i])))
+            print('Iter: {:8} train loss: {:.3f}'.format(i, float(loss_data[i])))
 
     return loss_data
 
