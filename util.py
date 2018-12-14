883c5b0d84d37c78c4af853219e9ad20659fe58a
pklinkmueller <pklinkmueller@jhu.edu>
2018-12-13 21:56:45 -0500

    Continue work on Logreg notebook. Add plotting utilities to util.py.

--- util.py
+++ util.py
@@ -23,8 +23,8 @@ def text_vectorize(x: DataFrame):
     vect = CountVectorizer()
     return vect.fit_transform(x)
 
-def plot_losses(gd, sgd_1, sgd_10, sgd_100, agd, svrg, md):
-    plt.figure( 1, figsize(14,10))
+def plot_fixed_losses(gd, sgd_1, sgd_10, sgd_100, agd, svrg, md):
+    plt.figure(figsize=(14,10))
     plt.xlabel('Iteration Number',fontsize='xx-large')
     plt.xlim(0,1000)
     plt.ylabel('Loss',fontsize='xx-large')
@@ -36,4 +36,17 @@ def plot_losses(gd, sgd_1, sgd_10, sgd_100, agd, svrg, md):
     plt.plot(svrg, 'r')
     plt.plot(md, 'm')
     plt.legend(['GD','SGD_1','SGD_10','SGD_100','AGD','SVRG','MD'], fontsize='x-large')
+    plt.show()
+
+def plot_dynamic_losses(gd, sgd_1, sgd_10, sgd_100, md):
+    plt.figure(figsize=(14,10))
+    plt.xlabel('Iteration Number',fontsize='xx-large')
+    plt.xlim(0,500)
+    plt.ylabel('Loss',fontsize='xx-large')
+    plt.plot(gd, '+' 'b')
+    plt.plot(sgd_1, 'g')
+    plt.plot(sgd_10, 'y')
+    plt.plot(sgd_100, 'k')
+    plt.plot(md, 'm')
+    plt.legend(['GD','SGD_1','SGD_10','SGD_100','MD'], fontsize='x-large')
     plt.show()
